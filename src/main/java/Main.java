f6c059e6b4bf1c584c0d63752667dc96f6d94cda
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-02-27 21:54:57 -0500

    changes made

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        int low = 1;
+        int high = 25;
+        int tries = 0;
+        boolean win = false;
+//        int numguess = 0
+        Scanner input = new Scanner(System.in);
+
+        Random random = new Random();
+        int answer = random.nextInt(low, high);
+//        System.out.println(answer);
+
+        while (!win) {
+            System.out.println("Guess a number between 1 - 25!");
+            int guess = input.nextInt();
+            tries++;
+            if (guess == answer) {
+                win = true;
+                System.out.println("YOU GOT IT!");
+                System.out.println("Number of tries: " + tries);
+                System.out.println("Correct number was: " + answer);
+            } else if (guess > answer) {
+                System.out.println("Answer is too large");
+
+            } else if (guess < answer) {
+                System.out.println("Answer is too small!");
+
+            }
+
+        }
     }
-}
+
+    }
