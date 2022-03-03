a503bcb21882b5cb44bd8172f9c3bf96de5896f5
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-03-02 23:22:15 -0500

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,32 +10,35 @@ public class Main {
         int low = 1;
         int high = 25;
         int tries = 0;
+        int memory = 0;
         boolean win = false;
-//        int numguess = 0
+
         Scanner input = new Scanner(System.in);
 
         Random random = new Random();
         int answer = random.nextInt(low, high);
-//        System.out.println(answer);
+
 
         while (!win) {
             System.out.println("Guess a number between 1 - 25!");
             int guess = input.nextInt();
-            tries++;
-            if (guess == answer) {
+
+            if (guess == memory) {
+                    System.out.println("Input was used");
+            } else if (guess > answer) {
+                System.out.println("Answer is too large");
+                tries++;
+
+            } else if (guess < answer) {
+                System.out.println("Answer is too small!");
+                tries++;
+
+            }else if (guess == answer) {
                 win = true;
                 System.out.println("YOU GOT IT!");
                 System.out.println("Number of tries: " + tries);
                 System.out.println("Correct number was: " + answer);
-            } else if (guess > answer) {
-                System.out.println("Answer is too large");
-
-            } else if (guess < answer) {
-                System.out.println("Answer is too small!");
-
-            }
-
         }
     }
 
-    }
+    }}
